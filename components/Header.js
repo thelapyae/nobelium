e1d43da391e2a836c4bb29e8bd7c3cfcd248eeef
thelapyae <97380388+thelapyae@users.noreply.github.com>
2022-06-11 15:43:54 +0630

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,9 @@ const NavBar = () => {
     { id: 0, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 1, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 2, name: locale.NAV.HOME, to: '/home', show: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
-    { id: 4, name: locale.NAV.RSS, to: '/feed', show: false }
+    { id: 3, name: locale.NAV.HOME, to: '/store', show: true },
+    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true },
+    { id: 5, name: locale.NAV.RSS, to: '/feed', show: false }
   ]
   return (
     <div className="flex-shrink-0">
